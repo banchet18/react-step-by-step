bdd5aa38c44c614c76be89b3f421bed45ccb3a24
banchet18 <banjarac6@gmail.com>
2023-12-14 13:09:37 +0530

    c

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Video from "./component/Video";
 
 const App = () => {
-  return <div>hello jee</div>;
+  return (
+    <>
+      <div>hello jee</div>
+      <Video />
+    </>
+  );
 };
 
 export default App;
