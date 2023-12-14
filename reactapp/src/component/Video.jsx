93abecdeae815d9bc3991c04b6b7ff4ef64a97fa
banchet18 <banjarac6@gmail.com>
2023-12-14 13:11:44 +0530

    c

--- reactapp/src/component/Video.jsx
+++ reactapp/src/component/Video.jsx
@@ -1,6 +1,11 @@
 import "./Video.css";
 
 const Video = () => {
-  return <div className="video">Video</div>;
+  return (
+    <div className="video">
+      <img src="https://loremflickr.com/500/400" alt="cats" />
+      <div>Learning React Component</div>
+    </div>
+  );
 };
 export default Video;
