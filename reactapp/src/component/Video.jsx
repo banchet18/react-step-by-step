ed214c3266633f0880edada62936a48b56df17b9
banchet18 <banjarac6@gmail.com>
2023-12-15 09:18:35 +0530

    c

--- reactapp/src/component/Video.jsx
+++ reactapp/src/component/Video.jsx
@@ -7,7 +7,7 @@ const Video = ({ title, channel, views, time, verified }) => {
         <img src="https://loremflickr.com/300/200" alt="cats" />
       </div>
       <div>{title}</div>
-      {channelJSX}
+      {verified ? <div>{channel}✅</div> : <div>{channel}</div>}
       <div>
         {views} views . {time}
       </div>
