8a967170449d47a8633c85bc9f0af55ceab80fc7
banchet18 <banjarac6@gmail.com>
2023-12-15 09:36:53 +0530

    c

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -3,13 +3,29 @@ import Video from "./component/Video";
 import Border from "./component/Border";
 
 const App = () => {
-  const obj = {
-    title: "React js",
-    channel: "coderDost",
-    views: "10k",
-    time: "1 year ago",
-    verified: false,
-  };
+  const videos = [
+    {
+      title: "React js",
+      channel: "coderDost",
+      views: "10k",
+      time: "1 year ago",
+      verified: false,
+    },
+    {
+      title: "Node js",
+      channel: "coderDost",
+      views: "100k",
+      time: "1 year ago",
+      verified: false,
+    },
+    {
+      title: "React js",
+      channel: "coderDost",
+      views: "10k",
+      time: "1 year ago",
+      verified: false,
+    },
+  ];
   return (
     <>
       <div className="container">
