f205e84ab50b8e847cf5ef096617d172d505d7b6
banchet18 <banjarac6@gmail.com>
2023-12-15 09:38:06 +0530

    c

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -5,25 +5,28 @@ import Border from "./component/Border";
 const App = () => {
   const videos = [
     {
+      id: 1,
       title: "React js",
       channel: "coderDost",
       views: "10k",
       time: "1 year ago",
-      verified: false,
+      verified: true,
     },
     {
+      id: 2,
       title: "Node js",
       channel: "coderDost",
       views: "100k",
-      time: "1 year ago",
+      time: "1 Month ago",
       verified: false,
     },
     {
-      title: "React js",
+      id: 3,
+      title: "Mongo DB",
       channel: "coderDost",
-      views: "10k",
-      time: "1 year ago",
-      verified: false,
+      views: "1M",
+      time: "1 day ago",
+      verified: true,
     },
   ];
   return (
