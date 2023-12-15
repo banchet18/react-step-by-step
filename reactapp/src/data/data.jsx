e14f94cbe1d3e6cc947e9e19355556bc3af883f9
banchet18 <banjarac6@gmail.com>
2023-12-15 10:07:10 +0530

    c

---
+++ reactapp/src/data/data.jsx
@@ -0,0 +1,26 @@
+const videos = [
+  {
+    id: 1,
+    title: "React js",
+    channel: "coderDost",
+    views: "10k",
+    time: "1 year ago",
+    verified: true,
+  },
+  {
+    id: 2,
+    title: "Node js",
+    channel: "coderDost",
+    views: "100k",
+    time: "1 Month ago",
+    verified: false,
+  },
+  {
+    id: 3,
+    title: "Mongo DB",
+    channel: "coderDost",
+    views: "1M",
+    time: "1 day ago",
+    verified: true,
+  },
+];
