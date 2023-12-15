e4848169a7a7917aad3b8e4cfa3d7557e7498081
banchet18 <banjarac6@gmail.com>
2023-12-15 09:09:34 +0530

    c

--- reactapp/src/component/Video.jsx
+++ reactapp/src/component/Video.jsx
@@ -1,13 +1,21 @@
 import "./Video.css";
 
 const Video = ({ title, channel, views, time }) => {
+  let verified = true;
+  let channelJSX;
+  if (verified) {
+    channelJSX = <div>{channel}✅</div>;
+  } else {
+    channelJSX = <div>{channel}</div>;
+  }
+
   return (
     <div className="">
       <div className="video">
         <img src="https://loremflickr.com/300/200" alt="cats" />
       </div>
       <div>{title}</div>
-      <div>{channel}</div>
+      {channelJSX}
       <div>
         {views} views . {time}
       </div>
